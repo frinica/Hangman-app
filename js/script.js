5676eadcb9d454f0715ad73f01586d71526e2944
frinica <frida.nicander@chasacademy.se>
2021-11-27 16:01:10 +0100

    add functions for displaying letters in boxes and initilaizing the game

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@
 // Globala variabler
 
 const wordList = [
-  "Vintergatan",
-  "Rymdraket",
-  "Astronaut",
-  "Solsystem",
-  "Meteorregn",
-  "Himlakropp",
-  "Konstellation",
-  "Supernova",
-  "Ljusår",
-  "Teleskop",
+  "VINTERGATAN",
+  "RYMDRAKET",
+  "ASTRONAUT",
+  "SOLSYSTEM",
+  "METEORREGN",
+  "HIMLAKROPP",
+  "KONSTELLATION",
+  "SUPERNOVA",
+  "LJUSÅR",
+  "TELESKOP",
 ]; // Array: med spelets alla ord
 
 let selectedWord; // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
@@ -21,16 +21,47 @@ let hangmanImg; // Sträng: sökväg till bild som kommer visas (och ändras) fe
 let msgHolderEl; // DOM-nod: Ger meddelande när spelet är över
 let startGameBtnEl = document.querySelector("#startGameBtn"); // DOM-nod: knappen som du startar spelet med
 let letterButtonEls = document.querySelectorAll("#letterButtons"); // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls; // Array av DOM-noder: Rutorna där bokstäverna ska stå
+let letterBoxEls = document.querySelector("#letterBoxes"); // Array av DOM-noder: Rutorna där bokstäverna ska stå
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-// Funktion som slumpar fram ett ord
-let randomWord = function (arr) {
-  let randomWord = Math.floor(Math.random() * arr.length);
-  selectedWord = arr[randomWord];
-  return selectedWord;
+const init = function () {
+  while (letterBoxEls.firstChild)
+    letterBoxEls.removeChild(letterBoxEls.firstChild);
+  randomWord(wordList);
+  createLetterBoxes();
 };
+
+// Funktion som slumpar fram ett ord
+const randomWord = function (arr) {
+  let random = Math.floor(Math.random() * arr.length);
+  selectedWord = arr[random];
+};
+
 // Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
+const createLetterBoxes = function () {
+  let letters = selectedWord.split("");
+  for (let i = 0; i < letters.length; i++) {
+    let li = document.createElement("li");
+    li.innerHTML = `<input type="text" disabled value="${letters[i]}" />`;
+    letterBoxEls.appendChild(li);
+  }
+};
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
+/* 
+- eventListener för var bokstav
+- Loopa igenom ordet för att se om bokstaven finns med
+- Om bokstaven finns ska den displayas
+- Om bokstaven inte finns ska en img läggas till
+- Bokstaven ska försvinna från borden 
+ */
+
 // Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
+/*
+- Om spelaren vinner ändra styling
+- Om spelaren förlorar ändra styling
+- Alla knappar förutom "starta spelet" ska disableas när spelaren vinner/förlorar
+*/
+
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+
+startGameBtnEl.addEventListener("click", init);
